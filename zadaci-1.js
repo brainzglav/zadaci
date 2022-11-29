f90765ad7ee641a3e74c1063260b81f93e982f2b
brainzglav <99499279+brainzglav@users.noreply.github.com>
2022-11-29 18:47:05 +0100

    Create zadaci-1.js
    Zadaci 1

---
+++ zadaci-1.js
@@ -0,0 +1,29 @@
+/* 
+Deklariraj varijablu broj i pridodijeli joj neki broj.
+Ako je broj 3 ili 7 ispisi: Broj je 3 ili 7
+Ako je manji od 3 ispisi: manji od 3
+Ako je broj veci od 3 a manji od 7 ispisi: Izmedu 3 i 7
+Ako je broj veci od 7 ispisi: Veci od 7
+*/
+
+/* 
+Deklariraj varijablu broj i pridodijeli joj neki broj.
+Ako je broj djeljiv s 3 ispisi Fizz
+Ako je djeljiv s 4 ispisi Buzz
+Ako je djeljiv s 3 i 4 ispisi Fizz Buzz
+Ako nije djeljiv ni s 3 ni 4  ispisi taj broj
+*/
+
+/* 
+Deklariraj varijablu myString i spremi u nju neki string.
+Uzmi posljednjih 5 znakova stringa i ispisi ih u sljedecem stringu:
+Posljednjih 5 znakova stringa je ____ 
+*/
+
+/* 
+Deklariraj varijablu myString i spremi u nju neki string.
+Uzmi sve znakove od 5. znaka pa do 10. znaka ne ukljucujuci 10. znak.
+Ako je duljina stringa manja od 5. ispisi: Krace od 5!
+Ako je string prazan ispisi: Prazan string!
+Ako je duljina stringa 5 ispisi te znakove
+*/
